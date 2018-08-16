2d83c7f8049be4d9afb517bdb15b0cf848fcc5c1
Jarren San Jose <sanjose.jarren@gmail.com>
2018-08-15 22:02:47 -0700

    add api key

--- spider-squid/src/app/services/create.service.ts
+++ spider-squid/src/app/services/create.service.ts
@@ -6,12 +6,14 @@ import { environment } from '../../environments/environment';
 @Injectable()
 export class CreateService {
   apiURL = environment.apiURL;
+  apiKey = environment.apiKey;
 
   constructor(public http:Http) { }
 
   create(text:string, voice:string) {
     var headers = new Headers();
     headers.append('Content-Type', 'application/json');
+    headers.append('x-api-key', this.apiKey);
     return this.http.post(this.apiURL, JSON.stringify({text:text, voice:voice}), {headers: headers})
       .map(res => res.json());
   }
--- spider-squid/src/environments/environment.ts
+++ spider-squid/src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://4o3ltrtba2.execute-api.us-east-1.amazonaws.com/dev'
+  apiURL: 'https://4o3ltrtba2.execute-api.us-east-1.amazonaws.com/dev',
+  apiKey: 'fP3GnVDveZ1f8DpRoCx6x4xHffqRQBk72OkHv5KO'
 };
